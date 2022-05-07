25c86640a5967b8ef5651886eac804e5d3d4fba9
Deepak Sirone Jegan <dsirone@cs.wisc.edu>
2022-05-07 13:22:38 -0500

    Add debug print statement

--- wolfcrypt/src/random.c
+++ wolfcrypt/src/random.c
@@ -1666,6 +1666,7 @@ static int wc_GenerateRand_IntelRD(OS_Seed* os, byte* output, word32 sz)
         int rand_gen_keystone(void);
         int rand_gen_keystone(void)
         {
+		printf("Call to GENRAND_WORD\n");
                 return SYSCALL_0(SYSCALL_GENRAND_WORD);
         }
 	#endif
